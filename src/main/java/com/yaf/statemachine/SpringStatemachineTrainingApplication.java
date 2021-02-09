7c0e340d6ac8f36c23af4764720a1abfeee4811d
Arda Fakılı <ardafakili@gmail.com>
2021-02-09 21:12:50 +0300

    Springboot project initialized

---
+++ src/main/java/com/yaf/statemachine/SpringStatemachineTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.yaf.statemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStatemachineTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStatemachineTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yaf/statemachine/SpringStatemachineTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaf.statemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStatemachineTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
